e93a1d8d8d72680967ed278f2a1a5f3cc5711095
onehunnitconst <skygana@gmail.com>
2023-09-28 15:39:06 +0900

    initial commit

---
+++ js/elements.js
@@ -0,0 +1,6 @@
+const shortnerInputContainer = document.getElementById('shortner-input-container');
+const shortnerResultContainer = document.getElementById('shortner-result-container');
+const shortnerInput = document.getElementById('shortner-input');
+const shortnerResult = document.getElementById('shortner-result');
+const shortnerSubmitButton = document.getElementById('shortner-submit-button');
+const shortnerCopyButton = document.getElementById('shortner-copy-button');
